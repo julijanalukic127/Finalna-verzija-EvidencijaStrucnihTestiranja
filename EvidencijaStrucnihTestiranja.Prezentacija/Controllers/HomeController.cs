92983a52dd3744af2ab3358bf3998ac0da167d49
julijanalukic127 <julijanalukic127@gmail.com>
2026-08-28 04:43:56 +0200

    Add files via upload

---
+++ EvidencijaStrucnihTestiranja.Prezentacija/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using EvidencijaStrucnihTestiranja.Prezentacija.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using EvidencijaStrucnihTestiranja.Prezentacija.Filteri;
+
+namespace EvidencijaStrucnihTestiranja.Prezentacija.Controllers
+{
+    /// <summary>
+    /// CRC:
+    /// Odgovornost: Upravlja prikazom početne stranice aplikacije.
+    /// Saradnici: Home pogledi.
+    /// </summary>
+    [ProveraSesijeFilter]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EvidencijaStrucnihTestiranja.Prezentacija/SOAP/ITestiranjeSoapServis.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceModel;
+using EvidencijaStrucnihTestiranja_Sloj_Servisa.DTO;
+
+namespace EvidencijaStrucnihTestiranja.Prezentacija.SOAP
+{
+    /// <summary>
+    /// CRC:
+    /// Odgovornost: Definiše SOAP operacije za pristup podacima o testiranjima.
+    /// Saradnici: TestiranjeSoapServis, TestiranjeDTO.
+    /// </summary>
+    [ServiceContract]
+    public interface ITestiranjeSoapServis
+    {
+        [OperationContract]
+        List<TestiranjeDTO> VratiSvaTestiranja();
+
+        [OperationContract]
+        TestiranjeDTO? PronadjiTestiranje(int id);
+    }
+}
